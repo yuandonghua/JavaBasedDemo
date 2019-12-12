d033b47369130204ae94eaf752cdb26811a2c1e6
袁东华 <1348474384@qq.com>
2019-12-12 15:00:41 +0800

    演示native

---
+++ bin/fit/hrkj/nativetest/HelloNative.c
@@ -0,0 +1,7 @@
+#include "hrkj_nativetest_HelloNative.h"
+#include <stdio.h>
+
+JNIEXPORT void JNICALL Java_fit_hrkj_nativetest_HelloNative_sayHello(JNIEnv * env, jobject obj)
+{
+	printf("Hello,JNI");
+}
