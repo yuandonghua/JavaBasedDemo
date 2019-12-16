818d9255f8bc2f156003c8da6a17d9888752522d
袁东华 <1348474384@qq.com>
2019-12-16 10:46:06 +0800

    测试

--- src/fit/hrkj/test/RowSetTest.java
+++ src/fit/hrkj/test/RowSetTest.java
@@ -5,7 +5,6 @@ import javax.sql.rowset.JdbcRowSet;
 import javax.sql.rowset.RowSetFactory;
 import javax.sql.rowset.RowSetProvider;
 
-
 /**
  * 2019年12月11日下午4:10:23<br>
  * 
@@ -35,26 +34,26 @@ public class RowSetTest {
 			// TODO Auto-generated catch block
 			System.out.println("驱动加载失败");
 			e1.printStackTrace();
-			
+
 		}
-			try (
-					//jdbc:mysql://127.0.0.1:3306/hrkj?useSSL=false&serverTimezone=UTC
+		try (
+				// jdbc:mysql://127.0.0.1:3306/hrkj?useSSL=false&serverTimezone=UTC
 				JdbcRowSet jrs = factory.createJdbcRowSet();) {
-				jrs.setUrl("jdbc:mysql://localhost:3306/hrkj?user=root&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true");
+			jrs.setUrl("jdbc:mysql://localhost:3306/hrkj?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true");
 //				jrs.setUrl("jdbc:mysql://localhost:3306/jhz?user=jhz&password=jhz-199339&serverTimezone=UTC");
 
-//				jrs.setUsername("root");
-				jrs.setPassword("1234qwer");
-				jrs.setCommand("select * from user");
+			jrs.setUsername("root");
+			jrs.setPassword("1234qwer");
+			jrs.setCommand("select * from user");
 //				jrs.setInt(1, 2);
-				jrs.execute();
-				TableFormatter tf = new TableFormatter(jrs);
-				tf.showResult();
-			} catch (Exception e) {
-				// TODO: handle exception
-				System.out.println("发生了异常");
-				e.printStackTrace();
-			}
+			jrs.execute();
+			TableFormatter tf = new TableFormatter(jrs);
+			tf.showResult();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("发生了异常");
+			e.printStackTrace();
+		}
 //		}else {
 //			System.out.println("数据库加载失败");
 //		}
