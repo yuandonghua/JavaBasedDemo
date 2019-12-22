c20538dfbf198ea79be669339e79b2ead1f3462b
袁东华 <1348474384@qq.com>
2019-12-22 13:03:49 +0800

    修复无法执行process方法的bug

--- src/fit/hrkj/annotation/apt/HiberNateAnnotationProcessor.java
+++ src/fit/hrkj/annotation/apt/HiberNateAnnotationProcessor.java
@@ -1,10 +1,7 @@
 package fit.hrkj.annotation.apt;
 
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.PrintStream;
-import java.util.Properties;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -17,6 +14,7 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Name;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
 
 /**
  * APT工具，用来生成映射文件<br>
@@ -26,23 +24,26 @@ import javax.lang.model.element.TypeElement;
  * @version 1.0
  */
 @SupportedSourceVersion(SourceVersion.RELEASE_11) // 指定版本
-@SupportedAnnotationTypes({ "Persistent", "Id", "Property" }) // 指定可处理的注解
+@SupportedAnnotationTypes({ "fit.hrkj.annotation.apt.Persistent", "fit.hrkj.annotation.apt.Id",
+		"fit.hrkj.annotation.apt.Property" }) // 指定可处理的注解,需要加上注解的包名
 public class HiberNateAnnotationProcessor extends AbstractProcessor {
 	@Override
 	public synchronized void init(ProcessingEnvironment processingEnv) {
 		// TODO Auto-generated method stub
 		super.init(processingEnv);
+		System.out.println("-----init-----");
 	}
 
 	// 循环处理每个需要处理的程序对象
-	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-		
+		if (!roundEnv.processingOver()) {
+			processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "process方法执行了");//打印报告
+		}
 		// TODO Auto-generated method stub
 		// 定义一个文件输出流，用于生成额外的文件
 		PrintStream ps = null;
 		try {
-			
+
 			// 遍历每个被@Persistent修饰的class文件
 			for (Element e : roundEnv.getElementsAnnotatedWith(Persistent.class)) {
 				// 获取正在处理的类名
@@ -50,16 +51,14 @@ public class HiberNateAnnotationProcessor extends AbstractProcessor {
 				// 获取类定义前的@Persistent注解
 				Persistent persistent = e.getAnnotation(Persistent.class);
 				// 创建文件输出流
-				ps = new PrintStream(new FileOutputStream("../files/"+clazzName + ".hbm.xml"));
+				ps = new PrintStream(new FileOutputStream("../files/" + clazzName + ".hbm.xml"));
 				// 执行输出
 				ps.println("<?xml version=\"1.0\"?>");
-				ps.println("<!DOCTYPE hibernate-mapping PUBLIC>");
+				ps.println("<!DOCTYPE hibernate-mapping PUBLIC");
 				ps.println("    \"-//Hibernate/Hibernate " + "Mapping DTD 3.0//EN\"");
-				ps.println("    \"http://www.hibernate.org/dtd/hibernate-mapping-3.0.dtd\"");
+				ps.println("    \"http://www.hibernate.org/dtd/hibernate-mapping-3.0.dtd\">");
 				ps.println("<hibernate-mapping>");
-				ps.println("    <class name=\"" + e);
-				// 输出persistent的table()值
-				ps.println("\"table=\"" + persistent.table() + "\">");
+				ps.println("    <class name=\"" + e+"\""+" table=\"" + persistent.table() + "\">");
 				for (Element e2 : e.getEnclosedElements()) {
 					// 只处理成员变量上的注解
 					if (e2.getKind() == ElementKind.FIELD) {// 1️⃣
