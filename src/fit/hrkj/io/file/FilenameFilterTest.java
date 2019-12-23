34f50c9c7927b811d22741a15af7970cc0869913
袁东华 <1348474384@qq.com>
2019-12-23 15:47:00 +0800

    演示过滤文件

---
+++ src/fit/hrkj/io/file/FilenameFilterTest.java
@@ -0,0 +1,29 @@
+package fit.hrkj.io.file;
+
+import java.io.File;
+
+/**
+ * 演示File类的list方法，过滤文件<br>
+ * 2019年12月23日下午3:03:57
+ * 
+ * @author 华软科技
+ * @version 1.0
+ */
+public class FilenameFilterTest {
+	/**
+	 * 程序入口
+	 * 
+	 * @param args 入口参数
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File f1 = new File("src/fit/hrkj/io/file");
+		// 使用Lambda表达式（目标类型为FilenameFilter）实现文件过滤器
+		// 如果文件名以.java结尾，或者文件对应一个路径，则返回true
+		String[] list = f1.list((dir, name) -> name.endsWith(".java") || new File(name).isDirectory());
+
+		for (String name : list) {
+			System.out.println(name);
+		}
+	}
+}
